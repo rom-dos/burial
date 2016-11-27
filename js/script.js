23188cca68c15759c17c534ebb6db4a2183bd653
Nicholas Matthew Morera <nicholasmatthewmorera@gmail.com>
2016-11-27 14:29:31 -0800

    1.0

--- js/script.js
+++ js/script.js
@@ -50,5 +50,65 @@ $(document).ready(function() {
 
   $('#burial-5 span').click(function() {
     $('#burial-5-info').slideToggle('slow');
-  })
+  });
+
+  $('#burial-7 span').click(function() {
+    $('#burial-7-info').slideToggle('slow');
+  });
+
+  $('#burial-8 span').click(function() {
+    $('#burial-8-info').slideToggle('slow');
+  });
+
+  $('#burial-9 span').click(function() {
+    $('#burial-9-info').slideToggle('slow');
+  });
+
+  $('#burial-10 span').click(function() {
+    $('#burial-10-info').slideToggle('slow');
+  });
+
+  $('#burial-11 span').click(function() {
+    $('#burial-11-info').slideToggle('slow');
+  });
+
+  $('#burial-12 span').click(function() {
+    $('#burial-12-info').slideToggle('slow');
+  });
+
+  $('#burial-13 span').click(function() {
+    $('#burial-13-info').slideToggle('slow');
+  });
+
+  $('#untrue-1 span').click(function() {
+    $('#untrue-1-info').slideToggle('slow');
+  });
+
+  $('#untrue-2 span').click(function() {
+    $('#untrue-2-info').slideToggle('slow');
+  });
+
+  $('#untrue-4 span').click(function() {
+    $('#untrue-4-info').slideToggle('slow');
+  });
+
+  $('#untrue-5 span').click(function() {
+    $('#untrue-5-info').slideToggle('slow');
+  });
+
+  $('#untrue-6 span').click(function() {
+    $('#untrue-6-info').slideToggle('slow');
+  });
+
+  $('#untrue-7 span').click(function() {
+    $('#untrue-7-info').slideToggle('slow');
+  });
+
+  $('#untrue-8 span').click(function() {
+    $('#untrue-8-info').slideToggle('slow');
+  });
+
+  $('#untrue-9 span').click(function() {
+    $('#untrue-9-info').slideToggle('slow');
+  });
 });
